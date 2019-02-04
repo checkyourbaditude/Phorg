fba08648e63e1af38726787466d3c6d96a360304
Chris <hello@chris-schaefer.me>
2019-02-04 13:32:46 -0800

    Got the database working with the program today, just need to iterate over the values and make sure all the values are being inserted into the database correctly

--- StatisticsScript.py
+++ StatisticsScript.py
@@ -4,18 +4,15 @@
 """
 
 import os
+import datetime
 import mysql.connector
 from mysql.connector import errorcode
+
 from PIL import Image
 from PIL.ExifTags import TAGS
 from collections import Counter
 
 def updateDatabase():
-
-	print "Made it to the updateDatabase function!"
-
-	#make a connection to the database
-	connectDatabase()
 
 	"""
 		Organize data by date, make list of objects per date
@@ -23,16 +20,15 @@ def updateDatabase():
 
 	#The top of the photo directory, this is going to have to be dynamically passed into the program through a box in the user UI
 	photoDirectory='C:/Users/Chris/Desktop/wrk'
+
 	#create a list of objects to be added to database
-
 	insertList=[]
-
-	'''
-	data1=photoData('DSC07943','C:/Users/Chris/Desktop/wrk/2019/01 Jan/04 Jan 2019/Highlights/DSC07943.JPG')
-	data2=photoData('DSC08252','C:/Users/Chris/Desktop/wrk/2019/01 Jan/19 Jan 2019/Highlights/DSC08252.JPG')
-	for attr, value in data1.__dict__.items():
-			print str(attr)+": "+str(value)	
-		'''
+	
+	insertList.append(photoData('DSC06287','C:/Users/Chris/Desktop/Phorg/organized/2018/11 November/17 Nov 2018/Highlights/DSC06287.JPG'))
+	#for attr, value in data1.__dict__.items():
+			#print str(attr)+": "+str(value)
+	
+	"""
 
 	#create the years list
 	yearsList=os.listdir(photoDirectory)
@@ -91,19 +87,23 @@ def updateDatabase():
 					#print "The file structure is incorrect, please make sure the Phorg application is being used correctly"
 					#sys.exit()
 
-	#Print out the list of objects to be inserted
+	#loop through the objects and insert them into the database, update the data in the database, or skip
 	for object in insertList:
 		print "\n"
 		for attr, value in object.__dict__.items():
 			print str(attr)+": "+str(value)
-			
+	
+	"""
+
+	#send the list to the insertDatabasefunction
+	insertDatabase(insertList)	
 
 
 
 #function that opens the database
-def connectDatabase():
+def insertDatabase(insertList):
 
-	print "Made it into the connect database function"
+	print "Made it into the insertDatabase function"
 
 	#connect to database
 	try:
@@ -113,6 +113,57 @@ def connectDatabase():
 			passwd="password",
 			database="PhotoData"
 		)
+
+		#open the cursor
+		dbCursor = dataBase.cursor()
+
+
+		#insert sql
+		#sqlPhotoHighlights = "INSERT INTO photoHighlights Highlight VALUES %s"
+
+		"""
+			Insert data into Database here
+		"""
+
+		#find the Camera Model and Lens model, then put them into the proper variables
+
+
+		#insert date if it is not entered yet
+		#print "Insert Date into database:"+str(insertList[0].Date)+"asdfasdf"
+
+		#queries for the lens and camera index
+		lensDataDB = "SELECT lensIndex FROM lensData WHERE fullLensName=%s"
+		cameraDataDB = "SELECT cameraIndex FROM cameraData WHERE cameraModel=%s"
+
+		#run query for the lens index
+		dbCursor.execute(cameraDataDB, (insertList[0].Model,))
+		cameraIndex=str(dbCursor.fetchall())
+		cameraIndex=cameraIndex[cameraIndex.find("(")+1:cameraIndex.find(",")]
+
+		#run query for the camera index
+		dbCursor.execute(lensDataDB, (insertList[0].LensModel,))
+		lensIndex=str(dbCursor.fetchall())
+		lensIndex=lensIndex[lensIndex.find("(")+1:lensIndex.find(",")]
+
+		"""
+			Seems like the brightness value is being rounded...
+		"""
+
+		#insert into the photoMetaData Table
+		v=(insertList[0].imageName, insertList[0].Date, insertList[0].FocalLength,insertList[0].FNumber,insertList[0].ExposureTime,insertList[0].ISOSpeedRatings,insertList[0].BrightnessValue,cameraIndex,lensIndex)
+		sqlPhotoMetaData = "INSERT INTO photoMetaData (photoName,photoDate,focalLength,aperture,shutterSpeed,ISO,BrightnessValue,cameraIndex,lensIndex) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+		dbCursor.execute(sqlPhotoMetaData, v)
+
+		#insert into the photoHighlights Table
+		#sqlPhotoHighlights = "INSERT INTO photoHighlights Highlight VALUES %s"
+
+		#commit the new data to the database
+		dataBase.commit()
+
+		dataBase.close()
+
+
+
 	except mysql.connector.Error as err:
 		if (err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
 			print "Your username or password is incorrect"
@@ -141,6 +192,10 @@ class photoData:
 		i = Image.open(image)
 		info = i._getexif()
 
+		"""
+			Exceptions need to be made if the file doesn't have exif data
+		"""
+
 		#loop through the exif data of the image, keeping the information then formatting it properly
 		for tag, value in info.items():
 
@@ -148,7 +203,9 @@ class photoData:
 
 			#inclusions of EXIF data, and reformating of the data
 			if(	decoded=='ISOSpeedRatings'): self.ISOSpeedRatings=str(value)
-			elif(decoded=='LensModel'): self.LensModel=str(value)
+			elif(decoded=='LensModel'):
+				self.LensModel=str(value)
+				print "LensModel: "+str(self.LensModel)
 			elif(decoded=='Make'): self.Make=str(value)
 			elif(decoded=='Model'): self.Model=str(value)
 			elif(decoded=='ExposureTime'):
@@ -183,9 +240,11 @@ class photoData:
 
 			elif(decoded=='DateTime'):
 
-				self.Year=		''.join(value[0:4])
-				self.Month=		''.join(value[5:7])
-				self.Day=		''.join(value[8:10])
+				Year=		''.join(value[0:4])
+				Month=		''.join(value[5:7])
+				Day=		''.join(value[8:10])
+
+				self.Date=str(datetime.date(int(Year), int(Month), int(Day)))
 
 		#figure out if the photo is a highlight and in the gallery
 		self.Highlight()
